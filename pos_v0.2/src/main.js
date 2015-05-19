8452400a19f3e4671152199cd5fdf63b430c5b4e
LiuYutian <l709178159@gmail.com>
2015-05-19 17:19:10 +0800

    pso_v0.2

--- pos_v0.2/src/main.js
+++ pos_v0.2/src/main.js
@@ -1,4 +1,31 @@
 //TODO: Please write code in this file.
 function printInventory(inputs) {
+    var array = loadAllItems();
 
+    for(var i = 0; i < array.length; i++) {
+        array[i].count = 0;
+    }
+
+    for(var i = 0; i < inputs.length; i++) {
+        for(var j = 0; j < array.length; j++) {
+            if(inputs[i] == array[j].barcode) {
+                array[j].count += 1;
+            }
+        }
+    }
+
+    var result = "***<没钱赚商店>购物清单***\n";
+    var sum = 0;
+    for(var i = 0; i < array.length; i++) {
+        if(array[i].count > 0) {
+            result += "名称：" + array[i].name + "，数量：" + array[i].count
+            + array[i].unit + "，单价：" + array[i].price.toFixed(2)+"(元)，小计："
+            + (parseInt(array[i].count)*parseInt(array[i].price)).toFixed(2)+"(元)\n";
+            sum += parseInt(array[i].count)*parseInt(array[i].price);
+        }
+    }
+    result += '----------------------\n'
+    result += "总计：" + sum.toFixed(2) + "(元)";
+    result += '\n**********************';
+    console.log(result);
 }
