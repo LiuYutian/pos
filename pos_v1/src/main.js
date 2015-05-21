3f0eca202e07035f138aaa6948955c218b125a6f
LiuYutian <l709178159@gmail.com>
2015-05-21 20:08:38 +0800

    vi change clean

--- pos_v1/src/main.js
+++ pos_v1/src/main.js
@@ -1,28 +1,39 @@
 function printInventory(inputs){
-    var arrayObject = loadAllItems();
-    var rule = loadPromotions()[0].barcodes;
+    var arrayObject = [];
+    for(var i = 0; i < inputs.length; i++) {
+        var inputsBarcode = inputs[i].indexOf("-") != -1 ? inputs[i].split("-")[0] : inputs[i];
+        var inputsNumber = inputs[i].indexOf("-") != -1 ? parseInt((inputs[i].split("-"))[1]) : 1;
+        var exist = false;
 
-    for(var i=0; i<arrayObject.length; i++) {
-        arrayObject[i].count = 0;
-        arrayObject[i].privilegeCount = 0;
+        for(var j = 0; j < arrayObject.length; j++) {
+            if(inputsBarcode == arrayObject[j].barcode) {
+                arrayObject[j].count = arrayObject[j].count || 0;
+                arrayObject[j].count += inputsNumber;
+                exist = true;
+                break;
+            }
+        }
+        if(!exist) {
+            arrayObject.push({barcode: inputsBarcode, count: inputsNumber, privilegeCount :0});
+        }
     }
 
-    for(var i=0; i<inputs.length; i++) {
-        var inputsBarcode = inputs[i].indexOf("-") != -1 ? inputs[i].split("-")[0] : inputs[i];
-        var inputsNumber = inputs[i].indexOf("-") != -1 ? parseInt((inputs[i].split("-"))[1]) : 1; 
-
-        for(var j=0; j<arrayObject.length; j++) {
-            if(inputsBarcode == arrayObject[j].barcode){
-                arrayObject[j].count += inputsNumber;
+    var loadAllItem = loadAllItems();
+    for(var i = 0; i < loadAllItem.length; i++) {
+        for(var j = 0; j < arrayObject.length; j++) {
+            if(arrayObject[j].barcode === loadAllItem[i].barcode) {
+                arrayObject[j].name = loadAllItem[i].name;
+                arrayObject[j].unit = loadAllItem[i].unit;
+                arrayObject[j].price = loadAllItem[i].price;
             }
         }
     }
 
-    var cheap = 0;
-    for(var i=0; i<arrayObject.length; i++) {
-        for(var j=0; j<rule.length; j++) {
-            if(arrayObject[i].barcode == rule[j] && arrayObject[i].count > 0) {
-                arrayObject[i].privilegeCount = Math.floor(arrayObject[i].count/3);
+    var rule = loadPromotions()[0].barcodes;
+    for(var i = 0; i < rule.length; i++) {
+        for(var j = 0; j < arrayObject.length; j++) {
+            if(rule[i] == arrayObject[j].barcode){
+                arrayObject[j].privilegeCount = Math.floor((arrayObject[j].count)/3);
             }
         }
     }
@@ -35,7 +46,6 @@ function printInventory(inputs){
             result += "数量：" + arrayObject[i].count + arrayObject[i].unit + "，";
             result += "单价：" + arrayObject[i].price.toFixed(2)+"(元)，";
             result += "小计：" + ((arrayObject[i].count-arrayObject[i].privilegeCount)*arrayObject[i].price).toFixed(2)+"(元)\n";
-
             sum += arrayObject[i].count*arrayObject[i].price;
         }
     }
@@ -45,7 +55,7 @@ function printInventory(inputs){
     var cheap = 0;
     for(var i=0; i<arrayObject.length; i++) {
         for(var j = 0; j < rule.length; j++) {
-            if(arrayObject[i].barcode == rule[j] && arrayObject[i].privilegeCount > 0) {
+            if(arrayObject[i].barcode == rule[j]) {
                 result += "名称："+arrayObject[i].name+"，";
                 result += "数量：" + arrayObject[i].privilegeCount + arrayObject[i].unit+"\n";
                 cheap += arrayObject[i].price;
